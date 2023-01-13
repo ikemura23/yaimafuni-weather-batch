8cadf281b1f7a0c2c5d4892ec80f1b504fc0cc52
ikemura23 <banbara.work@gmail.com>
2023-01-14 08:18:17 +0900

    feat: TimeSeriesWeatherMapper を JmaRepositoryに組み込む

--- src/main/kotlin/JmaRepository.kt
+++ src/main/kotlin/JmaRepository.kt
@@ -1,17 +1,23 @@
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
+import model.TimeSeriesWeatherItem
 
 /**
  * 気象庁のAPIと通信するRepository
  */
 class JmaRepository(
-    private val httpClientProvider: HttpClientProvider
+    private val httpClientProvider: HttpClientProvider,
+    private val mapper: TimeSeriesWeatherMapper = TimeSeriesWeatherMapper()
 ) {
     suspend fun fetch() {
         val client = httpClientProvider.create()
-        val httpResponse = client.get("https://www.jma.go.jp/bosai/jmatile/data/wdist/VPFD/474020.json")
+        val httpResponse = client.get("https://www.jma.go.jp/bosai/jmatile/data/wdist/VPFD/474010.json")
         println("httpResponse: $httpResponse")
         println("bodyAsText: ${httpResponse.bodyAsText()}")
-    }
 
+        val timeSeriesWeather = mapper.fromJson(httpResponse.bodyAsText())
+        println("timeSeriesWeather: $timeSeriesWeather")
+    }
 }
+
+
