f72a706ba10ba816ff438e6f78dd03f62238add6
ikemura23 <banbara.work@gmail.com>
2023-01-14 09:52:37 +0900

    refact: 不要なimportとコメントアウトを削除

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,14 +1,6 @@
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 
-fun main(args: Array<String>) {
-    println("Hello World!")
-
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+fun main() {
     val repository = JmaRepository(
         HttpClientProvider
     )
